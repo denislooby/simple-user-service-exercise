e8175c469f1c9e8b5c8b76f64bc956203f77a525
Denis Looby <denis@loobyonline.com>
2025-04-18 18:45:55 +0100

    Remove unnecessary script

--- run_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Set Python path to include user_service so imports work for tests
-export PYTHONPATH=./user_service
-
-echo "Running tests with PYTHONPATH=$PYTHONPATH"
-pytest tests
