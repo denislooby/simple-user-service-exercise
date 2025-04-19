346e4769d5fcbe9fa86cc69f2d1deb1801e5e3e6
Denis Looby <denis@loobyonline.com>
2025-04-19 18:08:42 +0100

    Prevent extra fields

--- src/user_service/user_models.py
+++ src/user_service/user_models.py
@@ -5,20 +5,32 @@ class UserCreate(BaseModel):
     email: EmailStr = Field(..., json_schema_extra={"example":"user@example.com"})
     name: str = Field(..., min_length=1)
     password: str = Field(..., min_length=6)
+    model_config = {
+        "extra": "forbid"
+    }
 
 class UserUpdatePut(BaseModel):
     name: str = Field(..., min_length=1)
     password: str = Field(..., min_length=6)
     last_login: Optional[str] = Field(..., description="Required field, but we'll take null") # Using Optional allows null while .. requires field
+    model_config = {
+        "extra": "forbid"
+    }
 
 
 class UserUpdatePatch(BaseModel):
     name: Optional[str] = Field(None, min_length=1)
     password: Optional[str] = Field(None, min_length=6)
     last_login: Optional[str] = None
+    model_config = {
+        "extra": "forbid"
+    }
 
 
 class LoginRequest(BaseModel):
     email: EmailStr
     password: str
+    model_config = {
+        "extra": "forbid"
+    }
     
